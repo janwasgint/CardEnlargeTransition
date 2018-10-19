ce00c0eaee0553ca62c21e5446064f81b9929f61
Jan Wasgint <jan.wasgint@quartett-mobile.de>
2018-10-19 23:28:41 +0200

    Initial commit

---
+++ CardEnlargingTransition/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CardEnlargingTransition
+//
+//  Created by Jan Wasgint on 19.10.18.
+//  Copyright © 2018 Jan Wasgint. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Framework/CardEnlargeAnimationController.swift
@@ -0,0 +1,9 @@
+//
+//  CardEnlargeAnimationController.swift
+//  CardEnlargingTransition
+//
+//  Created by Jan Wasgint on 19.10.18.
+//  Copyright © 2018 Jan Wasgint. All rights reserved.
+//
+
+import Foundation
